e0534803662b11c16a13847ed8bc2d3e0d390a64
tyzima <41972512+tyzima@users.noreply.github.com>
2023-09-08 23:47:56 -0400

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,41 +1,50 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let generateBtn = document.getElementById("generateBtn");
+console.log("Script loaded.");
 
-    if (generateBtn) {
-        generateBtn.addEventListener("click", function() {
-            let imageInput = document.getElementById("imageInput");
-            let colorInput = document.getElementById("colorsInput");
-            let outputArea = document.getElementById("outputArea");
-            
-            if (imageInput.files.length === 0) {
-                alert("Please upload an image first!");
-                return;
-            }
+let generateBtn = document.getElementById("generateBtn");
 
-            if (colorInput.value === "") {
-                alert("Please specify the number of colors!");
-                return;
-            }
+if (generateBtn) {
+    console.log("Button found.");
+    
+    generateBtn.addEventListener("click", function() {
+        console.log("Button clicked.");
 
-            let formData = new FormData();
-            formData.append("image", imageInput.files[0]);
-            formData.append("max_colors", colorInput.value);
+        let imageInput = document.getElementById("imageInput");
+        let colorInput = document.getElementById("colorsInput");
+        let outputArea = document.getElementById("outputArea");
+        
+        if (imageInput.files.length === 0) {
+            console.log("No image uploaded.");
+            alert("Please upload an image first!");
+            return;
+        }
 
-            fetch("https://api.vectorizer.io/v1/image-to-svg", {
-                method: "POST",
-                headers: {
-                    'Authorization': `Bearer ${process.env.VECTORIZER_API_KEY}`
-                },
-                body: formData
-            })
-            .then(response => response.text())
-            .then(data => {
-                outputArea.innerHTML = data;
-            })
-            .catch(error => {
-                console.error("Error fetching data:", error);
-                alert("An error occurred. Please try again later.");
-            });
+        if (colorInput.value === "") {
+            console.log("Colors not specified.");
+            alert("Please specify the number of colors!");
+            return;
+        }
+
+        let formData = new FormData();
+        formData.append("image", imageInput.files[0]);
+        formData.append("max_colors", colorInput.value);
+
+        fetch("https://api.vectorizer.io/v1/image-to-svg", {
+            method: "POST",
+            headers: {
+                'Authorization': `Bearer ${process.env.VECTORIZER_API_KEY}`
+            },
+            body: formData
+        })
+        .then(response => response.text())
+        .then(data => {
+            console.log("Data received:", data);
+            outputArea.innerHTML = data;
+        })
+        .catch(error => {
+            console.error("Error fetching data:", error);
+            alert("An error occurred. Please try again later.");
         });
-    }
-});
+    });
+} else {
+    console.log("Button not found.");
+}
